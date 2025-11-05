c96ab15630c5da0106d11d985d8d319cb300cdb5
changyeon47 <jms775845@gmail.com>
2025-11-05 14:49:44 +0900

    Add files via upload

---
+++ RCLCPP3/num1/pub.cpp
@@ -0,0 +1,26 @@
+#include"rclcpp/rclcpp.hpp" //헤더파일 포함
+#include"std_msgs/msg/string.hpp" //헤더파일 포함
+#include"rclcpp/time_source.hpp" //헤더파일 포함
+#include<memory> //헤더파일 포함
+#include<chrono> //헤더파일 포함
+using namespace std::chrono_literals; //std::chrono_literals생략
+void callback(rclcpp::Node::SharedPtr node,
+rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mypub)//함수 정의
+{
+    auto message = std_msgs::msg::String();//객체 생성
+    message.data = "Hello world!";//객체 맴버변수 값 초기화
+    RCLCPP_INFO(node->get_logger(), "Publish: %s", message.data.c_str());//콘솔창에 출력
+    mypub->publish(message);//발간
+}
+int main(int argc, char*argv[]) //메인함수
+{
+    rclcpp::init(argc, argv); //ros2 시스템 초기화
+    auto node=std::make_shared<rclcpp::Node>("node_pub1");//노드 객체 스마트포인터로 생성 
+    auto qos_profile=rclcpp::QoS(rclcpp::KeepLast(10)); //qos객체 생성
+    auto mypub=node->create_publisher<std_msgs::msg::String>("topic_pub1", qos_profile);//pub객체 생성
+    std::function<void()> fn = std::bind(callback, node, mypub);//함수호출할 콜백함수 생성
+    auto timer = node->create_wall_timer(100ms, fn);//타이머 객체 
+    rclcpp::spin(node);//노드 종료까지 무한 반복
+    rclcpp::shutdown(); //ros2시스템 종료
+    return 0; //함수 종료
+}
---
+++ RCLCPP3/num1/sub.cpp
@@ -0,0 +1,19 @@
+#include"rclcpp/rclcpp.hpp" //헤더파일 포함
+#include"std_msgs/msg/string.hpp" //헤더파일 포함
+#include<memory> //헤더파일 포함
+#include<functional> //헤더파일 포함
+void mysub_callback(rclcpp::Node::SharedPtr node, const std_msgs::msg::String::SharedPtr msg) //함수 정의
+{ 
+    RCLCPP_INFO(node->get_logger(), "Received message: %s", msg->data.c_str()); //콘솔창에 출력
+}
+int main(int argc, char*argv[]) //메인함수
+{ 
+    rclcpp::init(argc, argv); //ros2 시스템 초기화
+    auto node = std::make_shared<rclcpp::Node>("node_sub1");//node 객체 스마트 포인터 생성 
+    auto qos_profile= rclcpp::QoS(rclcpp::KeepLast(10)); //qos객체 생성
+    std::function<void(const std_msgs::msg::String::SharedPtr)> fn= std::bind(mysub_callback, node, std::placeholders::_1);// 호출할 콜백함수 생성
+    auto mysub= node->create_subscription<std_msgs::msg::String>("topic_pub1",qos_profile,fn); //sub객체 생성
+    rclcpp::spin(node); //노드 종료 명령까지 무한 반복
+    rclcpp::shutdown(); //ros2시스템 종료
+    return 0; //함수 종료
+}
