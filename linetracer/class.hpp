ce395f8d459754146c5bd86b4440bd67dbad7468
changyeon47 <jms775845@gmail.com>
2025-12-10 15:58:19 +0900

    Update class.hpp

--- linetracer/class.hpp
+++ linetracer/class.hpp
@@ -15,7 +15,6 @@ private:
 
     int components = 0;
 
-    // 파라미터들
     int target_brightness = 100;   // 목표 밝기
     int bin_threshold     = 130;   // 이진화 임계값
     int min_area          = 100;   // 최소 영역
@@ -24,12 +23,12 @@ private:
     cv::VideoWriter video_writer_; // 영상 저장용 (원하면 사용)
 
 public:
-    LineTracker() = default;
+    LineTracker();
 
     //전처리: BGR frame -> gray + 밝기 보정 + TH + 하단 1/4 ROI
-    void preprocess(const cv::Mat& frame);
+    void preprocess(cv::Mat& frame);
 
-    // --- 라벨링 ---
+    //라벨링
     void computeConnectedComponents();
 
     //기준점(pt)에 가장 가까운 객체 찾기
@@ -39,10 +38,10 @@ public:
     void drawObjects(bool found_target);
 
     // 에러 계산: 중앙 - pt.x
-    int computeError() const;
+    int computeError();
 
     // 처리된 ROI (색 입힌 이진 영상) 얻기
-    const cv::Mat& getThreshColor() const;
+    cv::Mat& getThreshColor();
 
     //(옵션) 원본 프레임을 비디오로 저장하고 싶을 때
     void writeFrame();
